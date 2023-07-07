455ce9815e0bbd3a106ec3a190c7b023235a989f
Nitin Ganwani <nitganwani@gmail.com>
2023-07-07 10:45:43 +0200

    Add changes to List component

--- src/features/components/list/List.tsx
+++ src/features/components/list/List.tsx
@@ -3,26 +3,43 @@ import { useFilms } from "../../hooks/use.films";
 import { Header } from "../header/Header";
 import style from "./List.module.scss";
 import { FilmCard } from "../film/FilmCard";
-// import { useSelector } from "react-redux";
-// import { RootState } from "../../../core/store/store";
+import { useUsers } from "../../hooks/use.users";
+import { useNavigate } from "react-router-dom";
 
 export default function List() {
   const { films, handleLoadFilms } = useFilms();
-  // const { currentUser, token } = useSelector((state: RootState) => state.users);
+  const { handleLogoutUser, token, currentUser } = useUsers();
+  const navigate = useNavigate();
 
   useEffect(() => {
     handleLoadFilms();
   }, [handleLoadFilms]);
 
+  const handleUser = () => {
+    handleLogoutUser();
+    navigate("/");
+  };
+
   return (
     <>
+      {token ? (
+        <>
+          <p>Hola {currentUser}</p>
+          <button onClick={() => navigate("/create")}>CREATE NEW FILM</button>
+          <button onClick={handleUser}>LOG OUT</button>
+        </>
+      ) : (
+        <p>Hola don Nadie</p>
+      )}
       <div className={style.list}>
         <Header title="OnlyFilms" subtitle="Feel your Films"></Header>
-        <ul>
-          {films.map((item) => (
-            <FilmCard key={item.id} item={item}></FilmCard>
-          ))}
-        </ul>
+        <div className={style.films}>
+          <ul>
+            {films.map((item) => (
+              <FilmCard key={item.id} item={item}></FilmCard>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
